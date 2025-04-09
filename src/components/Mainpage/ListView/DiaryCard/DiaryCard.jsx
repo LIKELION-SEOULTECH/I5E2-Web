207fd59ac2b37f5c43ae66e06c823d0334ff1c54
홍석주 <hsj202201@gmail.com>
2025-04-09 23:39:19 +0900

    fix: 날짜 수정

--- src/components/Mainpage/ListView/DiaryCard/DiaryCard.jsx
+++ src/components/Mainpage/ListView/DiaryCard/DiaryCard.jsx
@@ -2,6 +2,11 @@ import "../DiaryCard/DiaryCard.css";
 import { emotionInfo } from "/src/constants/emotionInfo";
 
 function DiaryCard({ data, onClick }) {
+  const formattedDate = new Date(data.createdAt).toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
   return (
     <div
       className="diary-card"
@@ -12,7 +17,7 @@ function DiaryCard({ data, onClick }) {
         <div className="top-icon">{emotionInfo[data.emotion].emoji}</div>
         <div className="top-info">
           <span className="username">{data.username}</span>
-          <span className="date">{data.date}</span>
+          <span className="date">{formattedDate}</span>
         </div>
       </div>
       <div className="cardcontent">{data.content}</div>
